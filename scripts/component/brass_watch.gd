d46a5ad61344fa515b08939e64ce5870e8dbe187
JohnathanRH <144834953+JohnathanRH@users.noreply.github.com>
2025-07-23 04:13:21 +0800

    Revert "interactibles and dialogs"
    This reverts commit 1cb80e2ffaa4721dc14f999bd47b6ba7c38b97e1.

--- scripts/component/brass_watch.gd
+++ scripts/component/brass_watch.gd
@@ -3,25 +3,12 @@ extends Item
 # Clean up task (if the time suffice):
 # Create state machine for each items
 
-@onready var player = self.get_parent()
-var battery : int
-
-func _ready() -> void:
-	battery = count_battery(player.inventory)
-
 func action() -> void:
 	# Pause game timer and start freeze timer
-<<<<<<< Updated upstream:scripts/component/brass_watch.gd
 	$"../timer".paused = true
 	$duration.start()
 	$CanvasLayer.visible = true
 
-=======
-	if(battery > 0):
-		$"../ingame_ui/timer".paused = true
-		$duration.start()
-		battery -= 1
->>>>>>> Stashed changes:script/brass_watch.gd
 
 func _unhandled_input(event: InputEvent) -> void:
 	# Freeze Trigger
@@ -39,10 +26,3 @@ func _process(delta: float) -> void:
 	# Display remaining frozen time
 	if($duration.is_stopped() == false):
 		$CanvasLayer/Label.text = "%.1f" % $duration.time_left
-
-func count_battery(inventory : PlayerInventory) -> int:
-	var count : int
-	for idx in inventory.items:
-		if(inventory.items[idx]["name"] == "battery"):
-			count += 1
-	return count
