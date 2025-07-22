31dbe4af10b01c08086060e53a4fe729c2d7fbee
JohnathanRH <144834953+JohnathanRH@users.noreply.github.com>
2025-07-23 05:11:01 +0800

    Interactables and Dialogs (Re-commit)

---
+++ scripts/resource/interactable.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name InteractableResource
+
+@export var name : String
+@export var sprite : CompressedTexture2D
+@export var texts : Array[String]    # Series of texts for the sake of story telling
+@export var isObjective : bool
